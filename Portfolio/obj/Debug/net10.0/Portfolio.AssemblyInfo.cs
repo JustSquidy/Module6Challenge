1028f90a497bd67b2d89f3f9a346d59f7297b620
Javier Reyes-Herrera <J4vierR3yes11@gmail.com>
2026-02-24 19:40:12 +0000

    adding a couple things I missed.

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296076ff388e3f54f27f288b746db35731c79f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1866e8198cf8ae8b96fc81610bab99ffdb0d461e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
